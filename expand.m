565ba13bd7ce78851361a61416482e69160df404
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-30 16:21:05 -0400

    From K columns adding all combinations
    Adding all combinations of the K columns, creating the rows H, and also creating a complement set.
    This was to avoid the non-robustness of column/row wise alpha/beta estimation. 
    It didn't work very well, but the problem lies elsewhere.

---
+++ expand.m
@@ -0,0 +1,35 @@
+function [Wexp, Hexp, notWexp, notHexp] = expand(W,X,fig_nr)
+
+% Expand the set to all combinations
+[n,d] = size(X);
+K = size(W,2)
+Zexp = W;
+for k = 2 : K-1
+  c = combnk(1:K,k);
+  for i = 1: size(c,1)
+    Zexp = [Zexp logical(sum(W(:,c(i,:)),2))];
+  end
+end
+Wexp = [Zexp logical(sum(W(:,1:K),2))];
+Hexp = calculate_h(X,Wexp);
+
+figure(fig_nr+1), subplot(1,3,1), imagesc(logical(Wexp*Hexp)), colormap(gray)  
+title(strcat('Initial A, K = ', num2str(size(W,2))))
+subplot(1,3,2), imagesc(Wexp), title('Initial columns')
+subplot(1,3,3), imagesc(Hexp), title('Initial rows')
+
+% Expand the set to all combinations
+Zexp = ~W;
+for k = 2 : K-1
+  c = combnk(1:K,k);
+  for i = 1: size(c,1)
+    Zexp = [Zexp ~logical(sum(W(:,c(i,:)),2))];
+  end
+end
+notWexp = [~Wexp ones(n,1)]; 
+notHexp = [Hexp; ~sum(Hexp)];
+
+figure(fig_nr+2)
+subplot(1,3,2), imagesc(notWexp), title('Initial columns')
+subplot(1,3,3), imagesc(notHexp), title('Initial rows')
+subplot(1,3,1), imagesc(notWexp*notHexp), title('Initial rows')
