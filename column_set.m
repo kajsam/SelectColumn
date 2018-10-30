bd19d27a37b5ecc647404ac3ddd62da08d3c3085
Kajsa Møllersen <kajsa.mollersen@uit.no>
2018-10-30 16:29:03 -0400

    Adjusting the penalty back and forth

--- column_set.m
+++ column_set.m
@@ -1,4 +1,4 @@
-function Zsets = column_set(Z, delete_noise)
+function Zsets = column_set(Z, delete_noise, fig_nr)
 
 % Creates a set of columns from Z for each single column of Z
 
@@ -19,12 +19,12 @@ for k =  1: nZ
   cand = [];
   for r = 1 : 100
     w_cand = [w Z(:,cand)];
-    w_or_cand = logical(sum(w_cand,2));
+    w_or_cand = sum(w_cand,2);
     pen = ones(1,nZ)*n;
     rew = zeros(1,nZ);
     crit = ones(1,nZ)*(-n);
     for i = setdiff(1:nZ,[k cand])
-      pen(i) = sum(w_or_cand & Z(:,i));  
+      pen(i) = sum((w_or_cand + Z(:,i))>3);  
       rew(i) = sum(~logical(w_or_cand) & Z(:,i));
       crit(i) = rew(i) - pen(i);
     end
@@ -35,7 +35,7 @@ for k =  1: nZ
     [maxcrit, idx] = max(crit);
      
     if maxcrit < 0
-      Zsets{k} = [k cand];
+      Zsets{k} = [k cand idx];
 %       if length(Zsets{k}) > 3
 %         [k cand]
 %       end
@@ -51,8 +51,13 @@ if delete_noise
 %      if ~isempty(del)
      %   figure(54), imagesc(Z(:,Zsets{k})), colormap(gray)
        Zsets{k}(del) = [];
- %     figure(53), imagesc(Z(:,Zsets{k})), colormap(gray), xlabel(k)
+     
+%     pause
 %   end
+end
+if fig_nr
+    figure(fig_nr), imagesc(Z(:,Zsets{k})), colormap(gray), xlabel(k)
+    title('Candidate set')
 end
       break
     else
